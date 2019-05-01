a9b484846b3da26b8f5477cdd015f8c9bfd66336
Richard Pentecost <richardpentecost1@gmail.com>
2019-05-01 23:10:43 +0100

    unstyled alert added to add property, and destructuring done

--- src/components/add-property.jsx
+++ src/components/add-property.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/add-property.css';
 import axios from 'axios';
+import Alert from './alert';
 
 const URL = 'http://localhost:3000/api/v1';
 
@@ -17,11 +18,19 @@ class AddProperty extends React.Component {
         city: 'Manchester',
         email: '',
       },
+      alertMessage: '',
+      isSuccess: false,
+      isError: false,
     };
   }
 
   handleAddProperty = (event) => {
     event.preventDefault();
+    this.setState({
+      alertMessage: '',
+      isSuccess: false,
+      isError: false,
+    });
     const { fields } = this.state;
     axios.post(`${URL}/PropertyListing/`, {
       title: fields.title,
@@ -34,9 +43,17 @@ class AddProperty extends React.Component {
     })
       .then(response => {
         console.log(response);
+        this.setState({
+          alertMessage: 'Property Added',
+          isSuccess: true,
+        });
       })
       .catch(error => {
         console.log(error, 'from error');
+        this.setState({
+          alertMessage: 'Server error, please try again',
+          isError: true,
+        });
       });
   };
 
@@ -50,13 +67,16 @@ class AddProperty extends React.Component {
   };
 
   render() {
+    const { fields, isSuccess, isError, alertMessage } = this.state;
     return (
       <div className="addProperty">
+        {isSuccess && <Alert message={alertMessage} />}
+        {isError && <Alert message={alertMessage} />}
         <form className="addPropertyForm" onSubmit={this.handleAddProperty}>
-          <label>Title:</label>
-          <input type="text" name="title" value={this.state.fields.title} onChange={this.handleFieldChange} placeholder="description" />
+          <label>Description:</label>
+          <input type="text" name="title" value={fields.title} onChange={this.handleFieldChange} placeholder="description" />
           <label>Type:</label>
-          <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+          <select name="type" value={fields.type} onChange={this.handleFieldChange}>
             <option value="Flat">Flat</option>
             <option value="Detached">Detached</option>
             <option value="Semi-detached">Semi-Detached</option>
@@ -66,15 +86,15 @@ class AddProperty extends React.Component {
             <option value="Bungalow">Bungalow</option>
           </select>
           <label>Bedrooms:</label>
-          <input type="number" name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} placeholder="No. of bedrooms" />
+          <input type="number" name="bedrooms" value={fields.bedrooms} onChange={this.handleFieldChange} placeholder="No. of bedrooms" />
           <label>Bathrooms:</label>
-          <input type="number" name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} placeholder="No. of bathrooms" />
+          <input type="number" name="bathrooms" value={fields.bathrooms} onChange={this.handleFieldChange} placeholder="No. of bathrooms" />
           <label>Price:</label>
-          <input type="number" name="price" value={this.state.fields.price} onChange={this.handleFieldChange} placeholder="Price" />
+          <input type="number" name="price" value={fields.price} onChange={this.handleFieldChange} placeholder="Price" />
           <label>email:</label>
-          <input type="string" name="email" value={this.state.fields.email} onChange={this.handleFieldChange} placeholder="Email" />
+          <input type="string" name="email" value={fields.email} onChange={this.handleFieldChange} placeholder="Email" />
           <label>City</label>
-          <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+          <select name="city" value={fields.city} onChange={this.handleFieldChange}>
             <option value="Manchester">Manchester</option>
             <option value="Leeds">Leeds</option>
             <option value="Sheffield">Sheffield</option>
